ec317aec5eba4a55b5f527906a56d0a59ae0c52e
Matt McFarland <matthew.mcfarland@creditcards.com>
2017-07-26 11:40:02 -0500

    Add VerifyToken plug and Claims helper module

---
+++ lib/keycloak/claims.ex
@@ -0,0 +1,36 @@
+defmodule Keycloak.Claims do
+  @moduledoc """
+  A helper module for extracting claims from the `%Joken.Token{}`. In order
+  to use these helpers, you **must** have the `VerifyToken` plug in your plug
+  pipeline.
+
+  ## Examples
+
+  #### Phoenix controller
+
+      def index(conn, _) do
+        sub = Keycloak.Claims.get_claim(conn, "sub")
+
+        conn
+        |> put_session(:user, sub)
+        |> render("index.html")
+      end
+  """
+
+  alias Plug.Conn
+  alias Joken.Token
+
+  @doc """
+  Pulls given `claim` from the Joken token
+  """
+  @spec get_claim(Plug.Conn.t | Joken.Token.t, String.t) :: String.t | nil
+  def get_claim(%Conn{assigns: %{token: token}}, claim),
+    do: get_claim(token, claim)
+
+  def get_claim(%Token{claims: claims}, claim) do
+    case claims do
+      %{^claim => value} -> value
+      _ -> nil
+    end
+  end
+end
---
+++ lib/keycloak/plug/verify_token.ex
@@ -0,0 +1,123 @@
+defmodule Keycloak.Plug.VerifyToken do
+  @moduledoc """
+  Plug for verifying authorization on a per request basis, verifies that a token is set in the
+  `Authorization` header.
+
+  ## Configuration
+
+      config :keycloak, Keycloak.Plug.VerifyToken,
+        hmac: "foo"
+
+  ## Examples
+
+      plug Keycloak.Plug.VerifyToken
+  """
+
+  import Plug.Conn
+
+  alias JOSE.JWK
+
+  @regex ~r/^Bearer:?\s+(.+)/i
+
+  @doc false
+  def init(opts), do: opts
+
+  @doc """
+  Fetches the `Authorization` header, and verifies the token if present. If a
+  valid token is passed, the decoded `%Joken.Token{}` is assigned as `:token`
+  in the `conn` assigns.
+  """
+  @spec call(Plug.Conn.t, keyword()) :: Plug.Conn.t
+  def call(conn, _) do
+    token =
+      conn
+      |> get_req_header("authorization")
+      |> fetch_token()
+
+    case verify_token(token) do
+      {:ok, token} ->
+        conn
+        |> assign(:token, token)
+      {:error, message} ->
+        conn
+        |> put_resp_content_type("application/vnd.api+json")
+        |> send_resp(401, Poison.encode!(%{error: message}))
+        |> halt()
+    end
+  end
+
+  @doc """
+  Attemps to verify that the passed `token` can be trusted
+
+  ## Examples
+
+      iex> verify_token(nil)
+      {:error, :not_authenticated}
+
+      iex> verify_token("abc123")
+      {:error, "Invalid signature"}
+  """
+  @spec verify_token(String.t | nil) :: {atom(), Joken.Token.t | atom()}
+  def verify_token(nil), do: {:error, :not_authenticated}
+  def verify_token(token) do
+    joken =
+      token
+      |> Joken.token()
+      |> Joken.with_validation("azp", &(&1 == signer_azp()), "Invalid azp")
+      |> Joken.with_validation("exp", fn(exp) ->
+        case DateTime.from_unix(exp) do
+          {:ok, dt} -> DateTime.compare(dt, DateTime.utc_now()) == :gt
+          {:error, _} -> false
+        end
+      end, "Invalid exp")
+      |> Joken.with_signer(signer_key())
+      |> Joken.verify()
+
+    case joken do
+      %{error: nil} -> {:ok, joken}
+      %{error: message} -> {:error, message}
+    end
+  end
+
+  @doc """
+    Fetches the token from the `Authorization` headers array, attempting
+    to match the token in the format `Bearer <token>`.
+
+    ### Example
+
+        iex> fetch_token([])
+        nil
+
+        iex> fetch_token(["abc123"])
+        nil
+
+        iex> fetch_token(["Bearer abc123"])
+        "abc123"
+  """
+  @spec fetch_token([String.t] | []) :: String.t | nil
+  def fetch_token([]), do: nil
+  def fetch_token([token | tail]) do
+    case Regex.run(@regex, token) do
+      [_, token] -> String.trim(token)
+      nil -> fetch_token(tail)
+    end
+  end
+
+  defp signer_key() do
+    case Application.get_env(:keycloak, __MODULE__, []) do
+      [hmac: hmac] ->
+        hmac
+        |> Joken.hs512()
+      [public_key: public_key] ->
+        public_key
+        |> JWK.from_pem()
+        |> Joken.rs256()
+      _ -> nil
+    end
+  end
+
+  defp signer_azp() do
+    Application.get_env(:keycloak, __MODULE__, [])
+    |> Keyword.get(:authorized_party, "keycloak")
+  end
+end
