b9c29df18bfa3acef8f6acb59aaacec017308070
Matt McFarland <783024+vanetix@users.noreply.github.com>
2020-05-08 09:57:21 -0500

    Add formatter and asdf config

---
+++ .formatter.exs
@@ -0,0 +1,8 @@
+locals_without_parens = []
+
+[
+  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  line_length: 100,
+  import_deps: [],
+  locals_without_parens: locals_without_parens
+]
